042feebd23a89ada6af4d54bcad89d51213040e4
Nitesh Kumar Chauhan <chauhannitesh19@gmail.com>
2024-07-22 23:39:51 +0530

    added datatype

---
+++ 01_index.js
@@ -0,0 +1,13 @@
+const accountId =123456;
+let accountEmail = "nitesh@gmaiil.com";
+var accountPassword ="Hello@1234";
+accountCity = "Jaipur";
+
+let accoutState;
+
+// accountId = 2;
+accountEmail ="hello@gmail.com";
+accountPassword = "123456789";
+accountCity = "Bangalore";
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accoutState]);
---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all JS code as newer version
+
+// alert(2+2) // we are using node JS not browser
+
+console.log(3+3);
+console.log("Nitesh");
+
+let name = "Nitesh";
+let age = 26;
+let IsloggedIn = false
+
+// Number => 2 to power 53
+// Bigint
+//String => ""
+// Boolean => true/false
+// Null => Standalone value
+// Undefined => 
+// Symbol => Unique
+
+
+
+//Object
+console.log(typeof null);
+console.log(typeof undefined);
